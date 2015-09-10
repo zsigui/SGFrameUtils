32f31c879a80b92b39edd1171ab2cd3813a36d51
zsigui <zsigui@sina.com>
2015-09-10 08:38:30 +0800

    update

---
+++ app/src/main/java/com/jackiezhuang/sgframework/utils/http/DownloadManager.java
@@ -0,0 +1,49 @@
+package com.jackiezhuang.sgframework.utils.http;
+
+import com.jackiezhuang.sgframework.utils.http.bean.HttpRequest;
+import com.jackiezhuang.sgframework.utils.http.itfc.IManager;
+
+import java.util.Collection;
+
+/**
+ * Created by JackieZhuang on 2015/9/10.
+ */
+public enum  DownloadManager implements IManager {
+
+	INSTANCE;
+
+	@Override
+	public void start() {
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+	@Override
+	public void add(HttpRequest request) {
+
+	}
+
+	@Override
+	public void addAll(Collection<HttpRequest> requests) {
+
+	}
+
+	@Override
+	public void cancel(HttpRequest request) {
+
+	}
+
+	@Override
+	public void cancelAll() {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
